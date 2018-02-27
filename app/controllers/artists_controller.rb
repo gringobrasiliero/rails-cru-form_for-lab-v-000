05d9e6ea430c527413b9ac4148192b32885e941d
Nolan Byrnes <nolanehs@gmail.com>
2018-02-27 09:28:02 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,13 +5,13 @@ class ArtistsController < ApplicationController
     @artist.save
     redirect_to artist_path(@artist)
   end
-   
+
   def update
     @artist = Artist.find(params[:id])
     @artist.update(artist_params(:name, :bio))
     redirect_to artist_path(@artist)
   end
-  
+
   def show
     @artist = Artist.all
   end
@@ -19,12 +19,12 @@ class ArtistsController < ApplicationController
   def new
     @artist = Artist.new
   end
-  
+
   def edit
     @artist = Artist.find(params[:id])
   end
 
-  
+
 private
   def artist_params(*args)
     params.require(:artist).permit(*args)
